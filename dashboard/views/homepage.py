7f8c0ae352381d90d1bf6b60631b58a7a3c93985
teaganlong3 <teagan.long3@gmail.com>
2026-07-31 19:48:02 -0400

    Fleshed out player analytics page, added an assets folder within the dashboard site for images and styling sheet

--- dashboard/views/homepage.py
+++ dashboard/views/homepage.py
@@ -1,6 +1,13 @@
 import duckdb
-import plotly.express as px
 import streamlit as st
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("dashboard/assets/styles.css")
 
 st.title("🏆 World Cup Analytics Dashboard")
 
@@ -28,57 +35,6 @@ df2 = conn.execute(
     from players
     """
 ).fetchdf()
-
-st.markdown(
-    """
-            <style>
-            /* Metric Card Container */
-            [data-testid="stMetric"] {
-                background-color: #f2f2f2;
-                border: 1px solid #d1d5db;
-                padding: 20px;
-                border-radius: 12px;
-                box-shadow: 2px 2px 8px rgba(0,0,0,0.88);
-                width: 100%;
-                text-align: center;
-            }
-            
-            /* Metric Title */
-            [data-testid="stMetricValue"] {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                width: 100%;
-                font-size: 1.2rem !important;
-                font-weight: bold !important;
-                color: #374151 !important;
-                text-align: center !important;
-                white-space: normal !important;
-                overflow-wrap: break-word !important;
-            }
-            
-            /* Metric Value */
-            [data-testid="stMetricLabel"] {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                width: 100%;
-                font-size: 2rem !important;
-                font-weight: bold !important;
-                color: #111827 !important;
-                text-align: center !important;
-                white-space: normal !important;
-                overflow-wrap: break-word !important;
-            }
-            
-            /* Remove Default Spacing */
-            [data-testid="stMetricDelta"] {
-                justify-content: center;
-            }
-            </style>
-            """,
-    unsafe_allow_html=True,
-)
 
 col1, col2, col3, col4 = st.columns([1.2, 1.2, 1.5, 1.5])
 col1.metric("⚽ Goals Scored", int(df1["home_score"].sum() + df1["away_score"].sum()))
--- dashboard/views/players.py
+++ dashboard/views/players.py
@@ -1,22 +1,77 @@
 import duckdb
+import pandas as pd
 import plotly.express as px
 import streamlit as st
+from st_keyup import st_keyup
 
 st.title("Player Analytics")
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("dashboard/assets/styles.css")
 
 conn = duckdb.connect("database/worldcup.duckdb")
 
 df = conn.execute(
     """
-    select
-    team,
-    player,
-    age,
-    goals,
-    assists
+    select *
     from players
     order by goals desc
     """
 ).fetchdf()
 
-st.dataframe(df)
+search = st_keyup("Search players:", key="search_query")
+
+if search:
+    result = df[df["player"].str.contains(search, case=False)]
+
+    st.write(f"Found {len(result)} players matching '{search}':")
+    st.dataframe(result, hide_index=True)
+else:
+    st.write("No player name provided. Displaying all players:")
+    st.dataframe(df, hide_index=True)
+
+
+metrics = st.text_input("Enter player name to view metrics:", key="metrics")
+
+if metrics:
+    player_data = df[df["player"].str.contains(metrics, case=False)]
+
+    if not player_data.empty:
+        bio_col, stat_col = st.columns([1, 2])
+
+        with bio_col:
+            st.subheader("Player Bio")
+            st.markdown(f"Name: {player_data.iloc[0]['player']}")
+            st.markdown(f"Country: {player_data.iloc[0]['team_country']}")
+            st.markdown(f"Club: {player_data.iloc[0]['club']}")
+            st.markdown(f"Position: {player_data.iloc[0]['position']}")
+            st.markdown(f"Age: {player_data.iloc[0]['age']}")
+            st.markdown(f"Birth Year: {player_data.iloc[0]['birth_year']}")
+
+        with stat_col:
+            st.subheader("Player Analytics")
+            mcol1, mcol2 = st.columns(2)
+            with mcol1:
+                with st.container():
+                    st.metric(label="Goals", value=int(player_data["goals"]))
+                with st.container():
+                    st.metric(label="Shots", value=int(player_data["shots"]))
+                with st.container():
+                    st.metric(
+                        label="Yellow Cards", value=int(player_data["cards_yellow"])
+                    )
+            with mcol2:
+                with st.container():
+                    st.metric(label="Passes", value=int(player_data["crosses"]))
+                with st.container():
+                    st.metric(label="Fouls", value=int(player_data["fouls"]))
+                with st.container():
+                    st.metric(label="Red Cards", value=int(player_data["cards_red"]))
+
+    else:
+        st.write(f"No data found for player '{metrics}'.")
